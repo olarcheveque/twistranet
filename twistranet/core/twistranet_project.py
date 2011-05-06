40e3fa2d68cdffe6d003ea06220637699db79554
macadames <jeanmat.grimaldi@gmail.com>
2011-05-06 14:58:25 +0200

    oups

--- twistranet/core/twistranet_project.py
+++ twistranet/core/twistranet_project.py
@@ -151,7 +151,7 @@ def twistranet_project():
         DEVEL_TWISTRANET_STATIC_PATH = os.path.abspath(os.path.join(theme_app_dir, 'static'))
         DEVEL_TWISTRANET_LOCALE_PATHS = "("
         for twapp in ('core', 'notifier', 'search', 'sharing', 'tagging', 'twistapp', ):
-            DEVEL_TWISTRANET_LOCALE_PATHS += "'%s', " %os.path.abspath(os.path.join(twist_package_path, twapp, 'locale'))
+            DEVEL_TWISTRANET_LOCALE_PATHS += "r'%s', " %os.path.abspath(os.path.join(twist_package_path, twapp, 'locale'))
         DEVEL_TWISTRANET_LOCALE_PATHS += ")"
         settings_path = os.path.join(project_path, "settings.py")
         f = open(settings_path, "r")
